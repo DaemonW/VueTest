72b693fb27d3e52b5ac908c5f6d884f0565b1a4c
daemonw <wangp@grt.com>
2021-10-27 14:45:21 +0800

    feat: set the video element to proper size

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div id="camera-frame">
         <video id="videoInput" class="canvas-big"></video>
       </div>
-      <div id="camera-rooter">
+      <div id="camera-footer">
         <div class="center">
           <button id="takePhotoButton" disabled>photo_camera</button>
         </div>
@@ -16,6 +16,7 @@
 
 <script>
 import ImageViewer from './components/ImageViewer.vue';
+
 let video;
 
 export default {
@@ -26,35 +27,52 @@ export default {
   data() {
     return {
       showGallery: false,
-      screenH:0,
-      screenW:0
+      screenH: 0,
+      screenW: 0
     }
   },
+  created() {
+    this.screenW = document.body.clientWidth;
+    this.screenH = document.body.clientHeight;
+    console.log(`width = ${this.screenW}, height = ${this.screenH}`);
+  },
   mounted() {
-    this.screenW = window.innerWidth;
-    this.screenH = window.innerHeight;
     video = document.getElementById('videoInput');
-    this.startRecord();
+    this.startRecord2();
   },
   methods: {
-    startRecord() {
+    async startRecord2() {
       if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        alert('不支持摄像设备');
         return;
       }
-
-      let temporaryConstraints = {
+      let devices = await navigator.mediaDevices.enumerateDevices();
+      devices.forEach(d=>{
+        console.log(`label = ${d.label}, group = ${d.groupId}, id = ${d.deviceId}`);
+      });
+      // let preferDevice = devices.find(d => {
+      //   if (d.deviceId === 'default' && d.groupId === 'default') {
+      //     return true;
+      //   }
+      //   return false;
+      // });
+      let ids = [
+          'aedf475a11f87171ddd0baa47fcf8c2a5f8e4e4b81c054fee3de81fb99d75380',
+          '0d131ca223654dd52ce8c996fb6cd030276b1596d1c7b0270603da68972cd351',
+          '066783ec655fb67b7d8107eb86d43e0de32f088302d6b5721b38d4a485bc6a24',
+          'f17bc172a9ddbf4e253e5299f2de0174f2487ce233d14c19660f3cdf216d996d'
+      ]
+      let constrains = {
         audio: false,
         video: {
-          // width: {min: 1280, ideal: 3840, max:3840},
-          // height: {min: 720, ideal: 2160, max:2160},
           width: {min: 960, ideal: 1920, max: 1920},
           height: {min: 640, ideal: 1080, max: 1080},
           facingMode: {ideal: "environment"},
-          focusMode: "continuous"
-          // aspectRatio: 16/9
-        }
-      };
-      navigator.mediaDevices.getUserMedia(temporaryConstraints)
+          focusMode: "continuous",
+          deviceId:ids[2]
+        },
+      }
+      navigator.mediaDevices.getUserMedia(constrains)
           .then(this.streamHandler())
           .catch(function (err) {
             console.log(err);
@@ -87,27 +105,24 @@ html, body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background: #000000;
   margin: 0;
   width: 100%;
   height: 100%;
 }
 
-#camera-frame {
-  height: 85vh;
-  width: 100vw;
-}
-
-#camera-rooter {
-  height: 15vh;
-  width: 100vw;
-}
-
-#videoInput{
-  object-fit: fill;
-  height: 85vh;
-  width: 100vw;
+#videoInput {
+  object-fit: cover;
   display: block;
+  height: 75vh;
+  width: 100vw;
+}
+
+#camera-footer {
+  display: block;
+  height: 10vh;
+  width: 100vw;
+  background: #000000;
 }
 
 .center {
