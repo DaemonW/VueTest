448c28bd3e52817f56fffac029e02f173659ffa1
daemonw <wangp@grt.com>
2021-10-27 12:58:45 +0800

    add video display

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,8 @@
 </template>
 
 <script>
-import ImageViewer from './components/ImageViewer.vue'
+import ImageViewer from './components/ImageViewer.vue';
+let video;
 
 export default {
   name: 'App',
@@ -24,8 +25,52 @@ export default {
   },
   data() {
     return {
-      showGallery: false
+      showGallery: false,
+      screenH:0,
+      screenW:0
     }
+  },
+  mounted() {
+    this.screenW = window.innerWidth;
+    this.screenH = window.innerHeight;
+    video = document.getElementById('videoInput');
+    this.startRecord();
+  },
+  methods: {
+    startRecord() {
+      if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        return;
+      }
+
+      let temporaryConstraints = {
+        audio: false,
+        video: {
+          // width: {min: 1280, ideal: 3840, max:3840},
+          // height: {min: 720, ideal: 2160, max:2160},
+          width: {min: 960, ideal: 1920, max: 1920},
+          height: {min: 640, ideal: 1080, max: 1080},
+          facingMode: {ideal: "environment"},
+          focusMode: "continuous"
+          // aspectRatio: 16/9
+        }
+      };
+      navigator.mediaDevices.getUserMedia(temporaryConstraints)
+          .then(this.streamHandler())
+          .catch(function (err) {
+            console.log(err);
+          });
+    },
+
+    streamHandler() {
+      return function (stream) {
+        video.srcObject = stream;
+        video.play();
+        self.video = video;
+        self.stream = stream;
+        //self.onCameraStartedCallback = callback;
+        //video.addEventListener('canplay', onVideoCanPlay, false);
+      }
+    },
   }
 }
 </script>
@@ -34,6 +79,7 @@ export default {
 html, body {
   width: 100%;
   height: 100%;
+  margin: 0;
 }
 
 #app {
@@ -42,17 +88,26 @@ html, body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 0;
+  margin: 0;
   width: 100%;
   height: 100%;
 }
 
 #camera-frame {
   height: 85vh;
+  width: 100vw;
 }
 
 #camera-rooter {
   height: 15vh;
+  width: 100vw;
+}
+
+#videoInput{
+  object-fit: fill;
+  height: 85vh;
+  width: 100vw;
+  display: block;
 }
 
 .center {
