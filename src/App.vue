f0b485be2cfad0424963b14ab732d2dce8b7462b
daemonw <wangp@grt.com>
2021-10-27 12:09:47 +0800

    init repo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="camera-panel">
+      <div id="camera-frame">
+        <video id="videoInput" class="canvas-big"></video>
+      </div>
+      <div id="camera-rooter">
+        <div class="center">
+          <button id="takePhotoButton" disabled>photo_camera</button>
+        </div>
+      </div>
+    </div>
+    <ImageViewer v-if="showGallery"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageViewer from './components/ImageViewer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImageViewer
+  },
+  data() {
+    return {
+      showGallery: false
+    }
   }
 }
 </script>
 
 <style>
+html, body {
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+  width: 100%;
+  height: 100%;
+}
+
+#camera-frame {
+  height: 85vh;
+}
+
+#camera-rooter {
+  height: 15vh;
+}
+
+.center {
+  margin: 0;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 }
 </style>
